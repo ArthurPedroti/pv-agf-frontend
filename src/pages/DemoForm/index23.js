387ff2f1e39ed717c499601af0d8bf201b1772bf
Arthur Pedroti Note <arthurpedroti@gmail.com>
2020-01-31 21:13:46 -0300

    catch-data-test

--- src/pages/DemoForm/index23.js
+++ src/pages/DemoForm/index23.js
@@ -44,7 +44,7 @@ const RenderInput = ({ input, meta, hintText, floatingLabelText }) => (
   </Container>
 );
 
-function FreightDetails({ prevEntrega, frete, formaPagEntrada }) {
+function FreightDetails({ vendedor, prevEntrega, frete, formaPagEntrada }) {
   return (
     <Container maxWidth="md" component="main" align="center">
       <MuiThemeProvider>
@@ -54,21 +54,18 @@ function FreightDetails({ prevEntrega, frete, formaPagEntrada }) {
             <Field
               name="prevEntrega"
               component={RenderInput}
-              defaultvalue={prevEntrega}
               hintText="Insira a previsão de entrega"
               floatingLabelText="Previsão de entrega"
             />
             <Field
               name="frete"
               component={RenderInput}
-              defaultvalue={frete}
               hintText="Insira o responsável pelo frete"
               floatingLabelText="Responsável pelo frete"
             />
             <Field
               name="formaPagEntrada"
               component={RenderInput}
-              defaultvalue={formaPagEntrada}
               hintText="Insira a previsão de instalação"
               floatingLabelText="Previsão de instalação"
             />
@@ -94,9 +91,7 @@ function FreightDetails({ prevEntrega, frete, formaPagEntrada }) {
 }
 
 const mapStateToProps = state => ({
-  step: state.pedidoInfos.step,
-  vendedor: state.pedidoInfos.vendedor,
-  naturezaOperacao: state.pedidoInfos.naturezaOperacao
+  vendedor: state.vendedor
 });
 
 FreightDetails = connect(mapStateToProps)(FreightDetails);
