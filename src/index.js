029d1b8539d78040c64d5eaaf971a55c6d7efdc8
Arthur Pedroti <arthurpedroti@gmail.com>
2020-07-22 08:54:13 -0300

    feat: sw skipwaiting

--- src/index.js
+++ src/index.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
-import { SW_INIT, SW_UPDATE } from './store/serviceWorker/types';
-import { store } from './store/index';
 
 import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
 serviceWorker.register({
-  onSuccess: () => store.dispatch({ type: SW_INIT }),
-  onUpdate: registration =>
-    store.dispatch({ type: SW_UPDATE, payload: registration }),
+  onUpdate: registration => {
+    const waitingServiceWorker = registration.waiting;
+
+    if (waitingServiceWorker) {
+      waitingServiceWorker.addEventListener('statechange', event => {
+        if (event.target.state === 'activated') {
+          window.location.reload();
+        }
+      });
+      waitingServiceWorker.postMessage({ type: 'SKIP_WAITING' });
+    }
+  },
 });
