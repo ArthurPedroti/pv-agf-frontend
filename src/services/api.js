6a0691fe7f7b1986d36fcdb18f4df17ed3ef5934
Arthur Pedroti <arthurpedroti@gmail.com>
2020-07-22 08:29:51 -0300

    feat: local mode

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,12 @@
 import axios from 'axios';
 
+const localMode = localStorage.getItem('@ASA');
+
 const api = axios.create({
-  baseURL: process.env.REACT_APP_SERVER,
+  baseURL:
+    localMode === 'on'
+      ? 'https://192.168.2.250/'
+      : process.env.REACT_APP_SERVER,
 });
 
 export default api;
